0445bc9daabe39f8de817d9f0df10f993a0ce000
AddisonGambhir <136495646+AddisonGambhir@users.noreply.github.com>
2023-07-31 21:34:42 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+#content of setup.py
+from setuptools import setup
+
+setup(name='my_package',
+      version='0.1',
+      description='A simple demonstration package',
+      url='https://github.com/AddisonGambhir/my_package',
+      author='Addison Gambhir',
+      author_email='ajg7pk@virginia.edu',
+      license='MIT',
+      packages=['my_package'])
